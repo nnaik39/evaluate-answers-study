0ce40819c923115ab800fc3033fac5cc0a653a4c
nnaik39 <nanni.naik@gmail.com>
2024-05-17 13:22:50 -0700

    updated study coverage

--- trial_info/check_study_coverage.py
+++ trial_info/check_study_coverage.py
@@ -1,6 +1,6 @@
 import json 
 
-data = json.load(open('/Users/nanditanaik/Downloads/ig-vqa-default-rtdb-evaluate-answers-study-export (1).json'))
+data = json.load(open('/Users/nanditanaik/Downloads/ig-vqa-default-rtdb-evaluate-answers-study-export (2).json'))
 pilot_exp = json.load(open('pilot_exp.json'))
 
 all_original_answers = json.load(open('all_answers.json'))
@@ -12,6 +12,11 @@ for participant in data:
         if ((trial['picture'], trial['category'], trial['question']) not in coverage_pair):
             coverage_pair[(trial['picture'], trial['category'], trial['question'])] = []
         coverage_pair[(trial['picture'], trial['category'], trial['question'])].append(trial['q1'])
+
+        if (trial['comments'] != ''):
+            print(trial['comments'])
+        if (trial['glb_comments'] != ''):
+            print(trial['glb_comments'])
 
 new_pilot_exp = {}
 new_pilot_exp['images'] = []
@@ -40,7 +45,7 @@ for answer in all_original_answers:
 
     if (not found):
         new_pilot_exp['images'].append(answer)
-        
+
 print("Number fully covered: ", num_covered)
 
 with open('new_pilot_exp.json', 'w') as f:
