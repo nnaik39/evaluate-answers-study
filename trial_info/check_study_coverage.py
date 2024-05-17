3215f2f275756dfa597255ba06538f8988796799
nnaik39 <nanni.naik@gmail.com>
2024-05-17 12:56:44 -0700

    updated study coverage

--- trial_info/check_study_coverage.py
+++ trial_info/check_study_coverage.py
@@ -1,15 +1,14 @@
 import json 
 
-data = json.load(open('/Users/nanditanaik/Downloads/ig-vqa-default-rtdb-evaluate-answers-study-export.json'))
+data = json.load(open('/Users/nanditanaik/Downloads/ig-vqa-default-rtdb-evaluate-answers-study-export (1).json'))
 pilot_exp = json.load(open('pilot_exp.json'))
 
-all_original_experiments = json.load(open('all_answers.json'))
+all_original_answers = json.load(open('all_answers.json'))
 
 coverage_pair = {}
 
 for participant in data:
     for trial in data[participant]:
-        print("trial: ", trial)
         if ((trial['picture'], trial['category'], trial['question']) not in coverage_pair):
             coverage_pair[(trial['picture'], trial['category'], trial['question'])] = []
         coverage_pair[(trial['picture'], trial['category'], trial['question'])].append(trial['q1'])
@@ -22,20 +21,27 @@ num_covered = 0
 for (img, ctxt, q) in coverage_pair:
     pilot_exp_entry = {}
     
-    for item in pilot_exp:
+    for item in pilot_exp['images']:
         if (item['filename'] == img and item['category'] == ctxt and item['question'] == q):
             pilot_exp_entry = item 
 
     if (len(coverage_pair[(img, ctxt, q)]) >= 3):
-        print("Ratings for an answer fully covered!")
-        print("image, ctxt, q: ", img, ctxt, q)
+        num_covered += 1
     else:
         new_pilot_exp['images'].append(pilot_exp_entry)
 
-print("Number of answers covered: ", len(coverage_pair))
+for answer in all_original_answers:
+    # If it's not in the coverage pair, then add it!
+    found = False 
+
+    for (img, ctxt, q) in coverage_pair:
+        if (answer['filename'] == img and answer['category'] == ctxt and answer['question'] == q):
+            found = True 
+
+    if (not found):
+        new_pilot_exp['images'].append(answer)
+        
+print("Number fully covered: ", num_covered)
+
 with open('new_pilot_exp.json', 'w') as f:
     f.write(json.dumps(new_pilot_exp, indent = 4))
-        # Remove it from pilot_exp
-# If an answer received more than 3 ratings, remove it from pilot_exp
-# Then upload new_pilot_exp.json!
-# 3 people seeing each answer
