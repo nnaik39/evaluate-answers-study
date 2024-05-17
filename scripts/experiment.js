353d2e7a015eab246d39d855ffbd354b4640f9df
nnaik39 <nanni.naik@gmail.com>
2024-05-17 12:18:28 -0700

    updated study coverage

--- scripts/experiment.js
+++ scripts/experiment.js
@@ -17,7 +17,7 @@ exp.customize = function() {
 
     imageSeenBefore = [];
 
-    main_trials = _.shuffle(main_trials)
+    main_trials = _.shuffle(main_trials)[0]
 
     console.log("Main trials ", main_trials)
 
